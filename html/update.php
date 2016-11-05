14232003f7a1f7d9c66f582201d9e52d4ff74b1b
Mark <pinchie@pinchie.net>
2016-11-05 20:18:44 +1100

    Update update.php

--- html/update.php
+++ html/update.php
@@ -1,5 +1,5 @@
 <?php
         echo '<html><head><title>Update in progress......</title></head><body>Updating RaspiPass...</body></html>';
-        exec('sudo git -C /git pull origin master > /raspipass/log/update.log && sudo /git/scripts/copy_to_sysdirs.sh >> /raspipass/log/update.log');
+        exec('sudo git -C /git pull origin master > /raspipass/log/update.log && sudo /git/scripts/copy_to_sysdirs.sh auto >> /raspipass/log/update.log');
 ?>
 
