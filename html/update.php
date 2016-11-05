31b3060d429a3c900f751249cfbae97fb6f5ebc0
Pinchie <chocobo9>
2016-11-05 21:03:59 +1100

    Typo

--- html/update.php
+++ html/update.php
@@ -1,6 +1,6 @@
 <?php
         echo '<html><head><title>Update in progress......</title></head><body>Updating RaspiPass...</body></html>';
-	echo('date > raspipass/log/update.log');
+	echo('date > /raspipass/log/update.log');
         exec('sudo git -C /git pull origin master >> /raspipass/log/update.log && sudo /git/scripts/copy_to_sysdirs.sh auto >> /raspipass/log/update.log && sudo chmod -Rv 777 /git/raspipass /git/raspi_secure /git/html /git/scripts >> /raspipass/log/update.log');
 ?>
 
