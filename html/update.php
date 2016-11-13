d78f8ac7e878491cca952859b094a4e1837c906e
Pinchie <pinchie@pinchie.net>
2016-11-14 10:18:16 +1100

    Amended update script to fix logging

--- html/update.php
+++ html/update.php
@@ -1,9 +1,12 @@
 <?php
-        echo '<html><head><title>Update in progress......</title></head><body>Updating RaspiPass...</body></html>';
-	exec('sudo date > /run/log/raspipass-update.log');
-        exec('sudo git -C /git pull origin master >> /run/log/raspipass-update.log && 
-		sudo /git/scripts/copy_to_sysdirs.sh auto >> /run/log/raspipass-update.log && 
-		sudo /git/scripts/set_ifconfig.sh >> /run/log/raspipass-update.log &&
-		sudo chmod -Rv 777 /git/raspipass /git/raspi_secure /git/html /git/scripts >> /run/log/raspipass-update.log');
+        echo '<html><head><title>Update in progress......</title></head><body>Updating RaspiPass...';
+	exec('sudo sh -c "date > /run/log/raspipass-update.log"');
+        exec('sudo sh -c "git -C /git pull origin master >> /run/log/raspipass-update.log" && 
+		sudo sh -c "/git/scripts/copy_to_sysdirs.sh auto >> /run/log/raspipass-update.log" && 
+		sudo sh -c "/git/scripts/set_interfaces.sh >> /run/log/raspipass-update.log" &&
+		sudo sh -c "chmod -Rv 777 /git/raspipass /git/raspi_secure /git/html /git/scripts >> /run/log/raspipass-update.log"');
+	echo '<br>Finished!';
+	echo '</body></html>';
+
 ?>
 
