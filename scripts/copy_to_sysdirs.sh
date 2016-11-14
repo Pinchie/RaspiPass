1700203f511e75b9ca2a707a4409a62290277ea5
Pinchie <pinchie@pinchie.net>
2016-11-14 16:02:49 +1100

    Permissions for script & Web GUI files/folders now stored in /scripts/filepermissions as csv to avoid editing copy_to_sysdirs.sh every time a file is added or deleted

--- scripts/copy_to_sysdirs.sh
+++ scripts/copy_to_sysdirs.sh
@@ -17,36 +17,54 @@ then
 	# /raspipass
 	echo Copying /git/raspipass...
 	rsync -vryW --exclude /raspipass/log/update.log --del --force /git/raspipass /
-	echo Setting permissions...
-	sudo chown -R raspi:raspi /raspipass
-	sudo chmod 664 /raspipass/config.ini
-	sudo chmod 664 /raspipass/hostapd.conf
-	sudo chmod 664 /raspipass/mac_addresses.txt
-	sudo chmod 664 /raspipass/mac_restrict.txt
-	sudo chmod 664 /raspipass/runchance.txt
-	sudo chmod 664 /raspipass/version
 	echo Done.
 	echo
 	
 	# /raspi_secure
 	echo Copying /git/raspi_secure...
 	sudo rsync -vryW --del --force /git/raspi_secure /
-	echo Setting permissions...
-	sudo chown -R root:root /raspi_secure
-	sudo chmod 644 /raspi_secure/firewall.rules
-	sudo chmod 744 /raspi_secure/iptables.sh
-	sudo chmod 744 /raspi_secure/raspipass
-	sudo chmod 744 /raspi_secure/clear_logs.sh
 	echo Done.
 	echo
 	
 	# /var/www/html
 	echo Copying /git/html...
 	sudo rsync -vryW --del --force /git/html /var/www/
-	echo Setting permissions...
-	sudo chown -R www-data:www-data /var/www/html
-	sudo chmod -R 755 /var/www/html
 	echo Done.
+
+	echo Setting permissions...
+	
+	IFS=","
+	while read file owner perm recurse
+	do
+		# Directories first
+		if [ -d "$file" ]
+		then
+			if [ $recurse == "TRUE" ]
+			then
+				chown -R $owner $file
+				chmod -R $perm $file
+			else
+				chown $owner $file
+				chmod $perm $file
+			fi
+		else
+	
+		# Then files and bad references
+			if [ -f "$file" ]
+			then
+				chown $owner $file
+				chmod $perm $file
+			else
+		# Ignore first line ($file="Location") and any blank links
+				if [ "$file" != "Location" ] && [ -n "$file" ]
+				then
+					echo ERROR: $file does not exist!
+				fi
+			fi
+		fi
+	done < /git/scripts/filepermissions
+
+
 else
 	echo
 	echo Exiting...
