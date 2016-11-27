d13171024df40c3c104577974ceafa23803475ed
Pinchie <pinchie@pinchie.net>
2016-11-28 10:20:26 +1100

    Typo fix

--- html/update.php
+++ html/update.php
@@ -4,7 +4,8 @@
 	exec('sudo sh -c "git -C /git pull origin master 2>&1 | tee -a /run/log/raspipass-update.log" && 
 		sudo sh -c "/git/scripts/copy_to_sysdirs.sh -a 2>&1 | tee -a /run/log/raspipass-update.log" &&
 		sudo sh -c "/git/scripts/set_interfaces.sh 2>&1 | tee -a /run/log/raspipass-update.log" &&
-		sudo sh -c "chmod -Rv 777 /git/raspipass /git/raspi_secure /git/html /git/scripts 2>&1 | tee -a /run/log/raspipass-update.log"');	echo '<br>Finished!';
+		sudo sh -c "chmod -Rv 777 /git/raspipass /git/raspi_secure /git/html /git/scripts 2>&1 | tee -a /run/log/raspipass-update.log"');
+	echo '<br>Finished!';
 	echo '</body></html>';
 
 ?>
