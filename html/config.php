973c104e6a4920f1041785a1b9fc4ced31924f97
Pinchie <pinchie@pinchie.net>
2017-04-16 14:30:27 +1000

    Added retrieval of locale list from /usr/share/zoneinfo

--- html/config.php
+++ html/config.php
@@ -24,6 +24,25 @@ if (file_exists('/raspipass/config.ini')) {
 $runsperhour=round(($config_array["runchance"]/100)/($config_array["runinterval"]/60),2);
 $smarty->assign('runsperhour',$runsperhour);
 
+// Get Locale region list
+$localeregions=array_filter(glob('/usr/share/zoneinfo/*'), 'is_dir');
+  // Remove unwanted entries
+$localeregions=array_diff($localeregions, array('/usr/share/zoneinfo/right', '/usr/share/zoneinfo/posix'));
+  // Get files from each region directory
+foreach($localeregions as $localeregion) {
+	$appendlocales=scandir($localeregion);
+	$appendlocales=array_diff($appendlocales, array('.', '..'));
+  // Save each entry as an absolute path
+	foreach($appendlocales as $appendlocale) {
+		$localepath[]=$localeregion . "/" . $appendlocale;
+	}
+}
+  // Clean up the leading /usr/share/zoneinfo/ for readability in drop-down
+$cleanlocales=str_replace("/usr/share/zoneinfo/","",$localepath);
+
+  // Assign resuls to smarty array
+$smarty->assign('locales',$cleanlocales);
+
 // Load dat template
 $smarty->display('config.tpl');
 ?>
