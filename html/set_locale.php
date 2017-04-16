47142a0a828605c2cfbaee41bb82e6cb88d94b91
Pinchie <pinchie@pinchie.net>
2017-04-16 20:21:12 +1000

    Finished locale setup script - ready for testing

--- html/set_locale.php
+++ html/set_locale.php
@@ -6,5 +6,13 @@ $locale=$cleanlocales[$_POST['Locale']];
 echo "Setting locale to $locale" . "<br>";
 echo "<br>";
 $absolutelocale="/usr/share/zoneinfo/" . $locale;
-echo "Path to locale is $absolutelocale" . "<br>";
+
+// Copy $absolutelocale to /etc/localtime
+// Put contents of $locale in /etc/timezone
+// Reconfigure with new timezone data
+exec('sudo sh -c "cp "' . $absolutelocale . '" /etc/localtime"');
+exec('sudo sh -c "echo "' . $locale . '" > /etc/timezone"');
+exec('sudo sh -c "dpkg-reconfigure --frontend=noninteractive tzdata > /dev/null 2>&1"');
+echo "Set to $locale - reloading configuration page";
+header('Location: index.php');
 ?>
